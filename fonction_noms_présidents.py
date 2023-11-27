68adff89d74296e4189b297af24c12895a23b608
edenelfassy <elfden.sup@yahoo.com>
2023-11-27 22:20:52 +0100

    première fonction

--- fonction_noms_présidents.py
+++ fonction_noms_présidents.py
@@ -9,3 +9,28 @@ def extraire_nom_president(nom_fichier):
 
     return nom_president
 
+def associer_prenom_a_president(nom_president):
+    prenoms = {
+        "Chirac": "Jacques",
+        "Giscard dEstaing": "Valéry",
+        "Hollande": "François",
+        "Macron": "Emmanuel",
+        "Mitterrand": "François",
+        "Sarkozy": "Nicolas"
+        # Ajoutez d'autres présidents au besoin
+    }
+
+    # Trouver le dernier mot dans le nom du président
+    mots = nom_president.split(" ")
+    nom_famille = mots[-1]
+
+    # Vérifier si le nom de famille est dans le dictionnaire
+    if nom_famille in prenoms:
+        prenom = prenoms[nom_famille]
+    else:
+        prenom = "Prénom non trouvé"
+
+    return prenom
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fonction_noms_présidents import extraire_nom_president
+from fonction_noms_présidents import extraire_nom_president, associer_prenom_a_president
 fichiers = [
     "Nomination_Chirac1.txt",
     "Nomination_Chirac2.txt",
@@ -13,3 +13,16 @@ fichiers = [
 for fichier in fichiers:
     nom_president = extraire_nom_president(fichier)
     print(f"Nom du président dans '{fichier}': {nom_president}")
+
+noms_presidents = [
+    "Chirac",
+    "Giscard dEstaing",
+    "Hollande",
+    "Macron",
+    "Mitterrand",
+    "Sarkozy"
+]
+
+for nom_president in noms_presidents:
+    prenom = associer_prenom_a_president(nom_president)
+    print(f"Prénom associé à {nom_president}: {prenom}")
